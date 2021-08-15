039726fe996ee6a0e8edfbc84c33c6db5bfec818
1azyfox <timyr.xas@gmail.com>
2021-08-15 16:04:24 +0300

    fixing JSON creating

--- src/main/java/services/jsonCreator/JSONFileCreator.java
+++ src/main/java/services/jsonCreator/JSONFileCreator.java
@@ -1,5 +1,7 @@
 package services.jsonCreator;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.*;
 import java.util.HashMap;
 import java.util.TreeMap;
@@ -13,10 +15,30 @@ public class JSONFileCreator {
 
     public void create(TreeMap<String, Long> treeMapOfCharacters) throws IOException {
         BufferedWriter bw = new BufferedWriter(new FileWriter(jsonFileName));
-        int i = treeMapOfCharacters.size() + 1;
-        while (i != 0) {
-            bw.write(JSONStringCreator.createJSONContent(treeMapOfCharacters));
-            i--;
+        ObjectMapper objectMapper = new ObjectMapper();
+        Treemap treemap = new Treemap();
+        treemap.treeMapOfCharacters = treeMapOfCharacters;
+        objectMapper.writeValue(new File(jsonFileName),treemap);
+//        for (int i = 0; i < 10; i++) {
+//            bw.write(objectMapper.writeValueAsString(treemap));
+//        }
+
+//        int i = treeMapOfCharacters.size() + 1;
+//        while (i != 0) {
+//            bw.write(JSONStringCreator.createJSONContent(treeMapOfCharacters));
+//            i--;
+//        }
+    }
+
+    class Treemap {
+        public TreeMap<String, Long> treeMapOfCharacters;
+
+        public TreeMap<String, Long> getTreeMapOfCharacters() {
+            return treeMapOfCharacters;
+        }
+
+        public void setTreeMapOfCharacters(TreeMap<String, Long> treeMapOfCharacters) {
+            this.treeMapOfCharacters = treeMapOfCharacters;
         }
     }
 }
